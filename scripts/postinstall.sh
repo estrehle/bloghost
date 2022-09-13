1177ff139cc05d5fdb29d6979d22a6905d7777c1
Elias Strehle <elias@circulartree.com>
2022-09-13 10:20:39 +0200

    add shebang

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
 
 cd ${SCRIPT_DIR}/service/ArticleService/application/layers/aws-sdk-v3/nodejs
 npm install
